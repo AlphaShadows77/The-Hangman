e58ba42f593c46727123df09eeaff2befd49ec5c
AlphaShadows77 <alpha-shadows@hotmail.com>
2019-05-15 21:51:14 +0200

    🎨 Comments modify

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ width = 300
 wd.configure(height=height, width=width)
 wd.resizable()
 
-# Image display
+# Image load and resize
 img_raw = Image.open("logo.jpg")
 img_raw = img_raw.resize((150, 150))
 
+# Image conversion
 image = ImageTk.PhotoImage(img_raw)
+
+# Image show
 can = Canvas(wd, height=height, width=width)
 can.create_image(width / 2, 50, image=image, anchor=N)
 can.pack()
