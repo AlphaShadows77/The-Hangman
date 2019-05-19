b11513a612f04186b1c3ac7e6d51241a4c1188a7
AlphaShadows77 <alpha-shadows@hotmail.com>
2019-05-19 16:25:37 +0200

    🎨 Place Geometry Manager use
    The Pack Geometry Manager is replaced by the Place Geometry Manager for the Button and the Canvas containing the logo of the game.

--- main.py
+++ main.py
@@ -9,23 +9,24 @@ wd.title("The Hangman")
 height = 300
 width = 200
 wd.configure(height=height, width=width)
-wd.resizable()
+wd.resizable(0, 0)
 
 # Image load and resize
 img_raw = Image.open("logo.jpg")
-img_raw = img_raw.resize((150, 150))
+img_height = 150
+img_width = 150
+img_raw = img_raw.resize((img_height, img_width))
 
 # Image conversion
 image = ImageTk.PhotoImage(img_raw)
 
 # Image show
-can = Canvas(wd, height=height, width=width)
-can.create_image(width / 2, 50, image=image, anchor=N)
-can.pack()
+can = Canvas(wd, height=img_height, width=img_width)
+can.create_image(0, 0, image=image, anchor=NW)
+can.place(anchor=N, x=width/2, y=50)
 
 # Play button
 play = Button(wd, text="Play !")
-play.pack()
-play.place(x=width/2, y=height-30, anchor=S)
+play.place(anchor=S, x=width/2, y=height - 30)
 
 wd.mainloop()
