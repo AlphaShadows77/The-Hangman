47542d500ed283929ae49bda1b3846b3ac025874
AlphaShadows77 <alpha-shadows@hotmail.com>
2019-06-02 13:48:28 +0200

    💄 Background set to white
    The background is set to white and the light grey border of the image is removed.

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from PIL import Image
 # Creation of the game Window
 wd = Tk()
 wd.title("The Hangman")
+wd.configure(bg="white")
 height = 300
 width = 200
 wd.configure(height=height, width=width)
 wd.resizable(0, 0)
 
 # Title print
-title = Label(wd, text="The Hangman", font=(None, -17, "bold"))
+title = Label(wd, bg="white", font=(None, -17, "bold"), text="The Hangman")
 title.place(anchor=N, x=width/2, y=15)
 
 # Image load and resize
@@ -25,7 +26,7 @@ img_raw = img_raw.resize((img_height, img_width))
 image = ImageTk.PhotoImage(img_raw)
 
 # Image show
-can = Canvas(wd, height=img_height, width=img_width)
+can = Canvas(wd, bg="white", height=img_height, highlightthickness=0, width=img_width)
 can.create_image(0, 0, image=image, anchor=NW)
 can.place(anchor=N, x=width/2, y=50)
 
